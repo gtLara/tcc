72e9c62ed7cc12dbb590b2f4eb2fc47f4cab6333
Gabriel Lara <gtlchaves@gmail.com>
2023-02-02 23:25:32 -0300

    Debugs destintion directory

--- code/get_images.sh
+++ code/get_images.sh
@@ -3,4 +3,4 @@
 figure_name=stationarity_examples.png
 figure_google_drive_id=1Kn_KMYytpqFp_2w8wTRtu-TnMms56rQi
 
-curl -L -o ../figures/$figure_name "https://docs.google.com/uc?export=download&id=${figure_google_drive_id}"
+curl -L -o figures/$figure_name "https://docs.google.com/uc?export=download&id=${figure_google_drive_id}" > /dev/null 2>&1
