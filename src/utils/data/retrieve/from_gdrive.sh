fc1f386e5570be7b40c4a589896eee0f6cfc251a
Gabriel Lara <gtlchaves@gmail.com>
2023-02-10 18:00:20 -0300

    Updates functional file ID

--- src/utils/data/retrieve/from_gdrive.sh
+++ src/utils/data/retrieve/from_gdrive.sh
@@ -4,7 +4,7 @@
 # @body same should be done for images!
 
 file_name=motor_current_samples.pkl
-file_google_drive_id=1hXXCtdgaoZqi_lKJ025RIzAljEcgosq
+file_google_drive_id=1g0BO8rbMFuX0C0aZyNV10hoD7IzjbBN4
 
 curl -L -o data/$file_name \
      "https://docs.google.com/uc?export=download&id=${file_google_drive_id}"
