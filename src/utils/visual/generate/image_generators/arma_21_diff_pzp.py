54d0fc05c10e9c3f15415b7f09303b61c505b0c5
Gabriel Lara <gtlchaves@gmail.com>
2023-03-03 20:40:54 -0300

    Adds new figure generators

---
+++ src/utils/visual/generate/image_generators/arma_21_diff_pzp.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from src.utils.visual.plot.pole_zero_diagram import pole_zero_plot
+
+
+def arma_21_diff_pzp(color, dpi, image_path, extension):
+
+    ma_coef = [-0.8, -0.2, 1]
+    ar_coef = [0.25, -0.5, 1]
+
+    pole_zero_plot(ma_coef, ar_coef)
+    plt.title(r"Diagrama de Polos e Zeros em $L$ de Processo ARMA(2, 1)")
+
+    path = f"{image_path}.{extension}"
+    plt.savefig(path, dpi=dpi, format=extension)
+    plt.close()
